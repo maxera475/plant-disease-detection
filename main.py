33f899dd05e48e097e8ece486d150ba5f3405064
Arpan Mishra <71602049+maxera475@users.noreply.github.com>
2024-12-27 16:45:42 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,75 @@
+from fastapi import FastAPI, File , UploadFile
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
+
+
+
+
+app = FastAPI()
+
+MODEL = tf.keras.models.load_model('my_model.keras')
+CLASS_NAMES = ['Apple___Apple_scab',
+ 'Apple___Black_rot',
+ 'Apple___Cedar_apple_rust',
+ 'Apple___healthy',
+ 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
+ 'Corn_(maize)___Common_rust_',
+ 'Corn_(maize)___Northern_Leaf_Blight',
+ 'Corn_(maize)___healthy',
+ 'Grape___Black_rot',
+ 'Grape___Esca_(Black_Measles)',
+ 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
+ 'Grape___healthy',
+ 'Pepper__bell___Bacterial_spot',
+ 'Pepper__bell___healthy',
+ 'Potato___Early_blight',
+ 'Potato___Late_blight',
+ 'Potato___healthy',
+ 'Tomato___Bacterial_spot',
+ 'Tomato___Early_blight',
+ 'Tomato___Late_blight',
+ 'Tomato___Leaf_Mold',
+ 'Tomato___Septoria_leaf_spot',
+ 'Tomato___Spider_mites Two-spotted_spider_mite',
+ 'Tomato___Target_Spot',
+ 'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
+ 'Tomato___Tomato_mosaic_virus',
+ 'Tomato___healthy']
+
+
+
+
+
+@app.get('/ping')
+async def ping():
+    return 'Hello, I am alive'
+
+
+
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+
+@app.post('/predict')
+async def predict(file: UploadFile):
+    image = read_file_as_image(await file.read())
+    
+    img_batch = np.expand_dims(image, 0)
+
+    prediction = MODEL.predict(img_batch)
+    predicted_class = CLASS_NAMES[np.argmax(prediction[0])]
+    confidence = np.max(prediction[0])
+    return {
+        'class' : predicted_class,
+        'confidence' : float(confidence)
+    }
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='localhost', port=8001)
